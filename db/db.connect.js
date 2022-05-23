a5e74283671b37616198fe3fd7e89cb51fecf488
unknown <jainprajwal123@gmail.com>
2022-05-23 21:46:49 +0530

    feat: db connection

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,10 +1,11 @@
 const mongoose = require(`mongoose`);
 
+// const PRODUCTION_URL = 
+
 const intiliazeDatabase = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@video-library-cluster.7y3yw.mongodb.net/test`
-    );
+    const PRODUCTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@video-library-cluster.7y3yw.mongodb.net/video-library`;
+    await mongoose.connect(PRODUCTION_URL);
     console.log(`db connection succesful!`);
   } catch (error) {
     console.error(`error connecting to database`, error);
