8cd61abbf2e7c358e5facdfb957e9667e3417bab
unknown <jainprajwal123@gmail.com>
2022-05-23 21:48:07 +0530

    imported required modules

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const PORT = 8081;
 const express = require(`express`);
 const { intiliazeDatabase } = require("./db/db.connect");
-
 const app = express();
-const dotenv = require(`dotenv`)
+const dotenv = require(`dotenv`);
+const cors = require(`cors`);
+
+const { router: videos } = require(`./routers/videos.router`);
 
 
-dotenv.config()
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+
 intiliazeDatabase();
-
 app.get(`/`, (req, res) => {
   res.json({
     success: true,
     message: `up and running`,
   });
 });
+
+app.use(`/videos`, videos);
 
 app.listen(PORT, () => {
   console.log(`running on port`, PORT);
