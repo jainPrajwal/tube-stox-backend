5bfd86c6a2ab532e5e0142a898e4333d02624ced
unknown <jainprajwal123@gmail.com>
2022-05-25 22:12:32 +0530

    feat: update playlist metadata, delete playlist, add video to specified playlist, delete video from a specified playlist

--- routers/playlists.router.js
+++ routers/playlists.router.js
@@ -1,19 +1,26 @@
 const express = require(`express`);
 const { PlaylistModel } = require("../models/playlist.model");
+const { VideoModel } = require("../models/video.model");
 const { RESPONSE } = require("../utils/common.utils");
 
 const router = express.Router();
 
-
 router
   .route(`/`)
   .get(async (req, res) => {
+    const { user } = req;
+
+    if (!user) {
+      throw new Error(`User not found in playlists`);
+    }
     try {
-      const playlists = await PlaylistModel.find({}).populate(`videos`);
+      const playlists = await PlaylistModel.find({ owner: user._id }).populate(
+        `videos`
+      );
       res.json({
         status: 200,
         success: true,
-        message: `play lists fetched successfully`,
+        message: `playlists fetched successfully`,
         playlists,
       });
     } catch (error) {
@@ -26,9 +33,27 @@ router
     }
   })
   .post(async (req, res) => {
-    const { playlist } = req.body;
+    const {
+      body: { playlist },
+      user,
+    } = req;
+
+    console.log(`user`, user);
+    if (!user) {
+      throw new Error(`User not found in playlists`);
+    }
     try {
       if (playlist) {
+        if (playlist.type !== `custom`) {
+          res.json({
+            status: 400,
+            success: false,
+            message: `Playlist is of type default playlist`,
+          });
+          return;
+        }
+        playlist.owner = user._id;
+        playlist.videos = [];
         let savedPlaylist = await new PlaylistModel(playlist).save();
         if (savedPlaylist) {
           res.json({
@@ -53,4 +78,206 @@ router
     }
   });
 
+router
+  .route(`/:playlistId`)
+  .post(async (req, res) => {
+    const {
+      body: { playlist },
+      user,
+      params: { playlistId },
+    } = req;
+    try {
+      const foundPlaylist = await PlaylistModel.findOneAndUpdate(
+        { _id: playlistId, isDefault: false },
+        { ...playlist },
+        { new: true }
+      );
+      if (!foundPlaylist) {
+        res.json({
+          ...RESPONSE.NOT_FOUND,
+          message: `playlist ${playlistId} not found`,
+        });
+        return;
+      }
+      res.json({
+        status: 201,
+        success: true,
+        message: `playlist updated successfully`,
+        playlist: foundPlaylist,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `something went wrong while updating the playlist`,
+        errorMessage: error.message,
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    const {
+      user,
+      params: { playlistId },
+    } = req;
+    try {
+      const foundPlaylist = await PlaylistModel.findOneAndDelete({
+        _id: playlistId,
+        isDefault: false,
+      });
+      if (!foundPlaylist) {
+        res.json({
+          ...RESPONSE.NOT_FOUND,
+          message: `playlist ${playlistId} not found`,
+        });
+        return;
+      }
+      res.json({
+        status: 200,
+        success: true,
+        message: `Playlist deleted sucessfully`,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `somehting went wrong while deleting the playlist`,
+        errorMessage: error.message,
+      });
+    }
+  });
+
+router
+  .route("/:playlistId/videos/:videoId")
+  .post(async (req, res) => {
+    const {
+      params: { playlistId, videoId },
+    } = req;
+
+    try {
+      const foundPlaylist = await PlaylistModel.findOne({
+        _id: playlistId,
+       
+      });
+      const video = await VideoModel.findOne({ _id: videoId });
+      let updatedPlaylist;
+
+      if (!foundPlaylist) {
+        res.json({
+          ...RESPONSE.NOT_FOUND,
+          message: `Playlist not found!`,
+        });
+        return;
+      }
+      if (!video) {
+        res.json({
+          ...RESPONSE.NOT_FOUND,
+          message: `Video not found!`,
+        });
+        return;
+      }
+
+      if (foundPlaylist.type === `liked`) {
+        if (foundPlaylist.videos.some((id) => id.toString() === videoId)) {
+          foundPlaylist.videos = foundPlaylist.videos.filter(
+            (id) => id.toString() !== videoId
+          );
+        } else {
+          foundPlaylist.videos = [...foundPlaylist.videos, videoId];
+        }
+      } else {
+        if (foundPlaylist.videos.some((id) => id.toString() === videoId)) {
+          foundPlaylist.videos = [video._id];
+        } else {
+          if (foundPlaylist.type === `history`) {
+            foundPlaylist.videos = [videoId, ...foundPlaylist.videos];
+          } else {
+            foundPlaylist.videos = [...foundPlaylist.videos, videoId];
+          }
+        }
+      }
+      updatedPlaylist = await foundPlaylist.save();
+
+      res.json({
+        status: 201,
+        success: true,
+        message: `Playlist Updated Succesfully`,
+        playlist: await updatedPlaylist.populate("videos"),
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `somehting went wrong while fetching playlsit from DB`,
+        errorMessage: error.message,
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    const {
+      user,
+      params: { playlistId, videoId },
+    } = req;
+    const foundPlaylist = await PlaylistModel.findOne({
+      _id: playlistId,
+    });
+    if (!foundPlaylist) {
+      res.json({
+        ...RESPONSE.NOT_FOUND,
+        message: `Playlist not found!`,
+      });
+      return;
+    }
+    const video = await VideoModel.findOne({ _id: videoId });
+    if (!video) {
+      res.json({
+        ...RESPONSE.NOT_FOUND,
+        message: `Video not found!`,
+      });
+      return;
+    }
+
+    const updatedVideosList = foundPlaylist.videos.filter(
+      (_id) => _id.toString() !== videoId
+    );
+    foundPlaylist.videos = updatedVideosList;
+    const updatedPLaylist = await foundPlaylist.save();
+    res.json({
+      status: 200,
+      success: true,
+      message: `Video Deleted Successfully`,
+      playlist: updatedPLaylist,
+    });
+  });
+
+router.route(`/:type/videos`).get(async (req, res) => {
+  const {
+    params: { type },
+    user,
+  } = req;
+
+  try {
+    const foundPlaylist = await PlaylistModel.find({
+     owner: user._id,
+     type
+    }).populate("videos");
+
+    if (foundPlaylist.length <= 0) {
+      res.json({
+        ...RESPONSE.NOT_FOUND,
+        message: `type not found in your playlist`,
+      });
+      return;
+    }
+
+    res.json({
+      status: 200,
+      success: true,
+      message: `Playlist fetched successfully from the DB`,
+      playlist: foundPlaylist,
+    });
+  } catch (error) {
+    res.json({
+      ...RESPONSE.INTERNAL_SERVER_ERROR,
+      message: `somehting went wrong while fetching playlsit from DB`,
+      errorMessage: error.message,
+    });
+  }
+});
 module.exports = { router };
