852cdbb815af188b86bd477b2c4e7237cf87a9e6
unknown <jainprajwal123@gmail.com>
2022-05-25 22:27:24 +0530

    feat: login user

--- routers/login.router.js
+++ routers/login.router.js
@@ -1,23 +1,46 @@
 const express = require(`express`);
 const jwt = require(`jsonwebtoken`);
+const bcrypt = require(`bcrypt`);
+const { UserModel } = require("../models/user.model");
 const { RESPONSE } = require(`../utils/common.utils`);
 const router = express.Router();
 
-router.route(
-  "/".post(async (req, res) => {
-    let { user } = req.body;
-    if (user) {
-      try {
-        //   const user = 
-      } catch (error) {
+router.route("/").post(async (req, res) => {
+  let { user } = req.body;
+  if (user) {
+    try {
+      const foundUser = await UserModel.findOne({email: user.email});
+   
+      const isPasswordValid = await bcrypt.compare(
+        user.password,
+        foundUser.password
+      );
+      if (!isPasswordValid) {
         res.json({
-          ...RESPONSE.INTERNAL_SERVER_ERROR,
-          message: `something wen wrong while loggin in..`,
-          errorMessage: error.message,
+          ...RESPONSE.UNAUTHENTICATED_USER,
+          message: `Invalid Password`,
         });
+        return;
       }
-    } else {
-      res.json(RESPONSE.MALFORMED_SYNTAX);
+
+      const token = jwt.sign({ _id: foundUser._id }, process.env.mySecret);
+      res.json({
+        status: 201,
+        success: true,
+        message: `Login Successful`,
+        token,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `something went wrong while loggin in..`,
+        errorMessage: error.message,
+      });
     }
-  })
-);
+  } else {
+    res.json(RESPONSE.MALFORMED_SYNTAX);
+  }
+});
+
+
+module.exports = {router}
