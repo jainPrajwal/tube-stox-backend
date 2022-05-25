caf30c2dfed126ab8c86353a3fe4092ec7d7ac73
unknown <jainprajwal123@gmail.com>
2022-05-25 22:27:47 +0530

    feat: signup user

--- routers/signup.router.js
+++ routers/signup.router.js
@@ -3,13 +3,16 @@ const bcrypt = require(`bcrypt`);
 const jwt = require(`jsonwebtoken`);
 const { UserModel } = require("../models/user.model");
 const { RESPONSE } = require("../utils/common.utils");
+const { PlaylistModel } = require("../models/playlist.model");
 const router = express.Router();
 
 router.route(`/`).post(async (req, res) => {
   let { user } = req.body;
   try {
     if (user) {
-      const userWithSameEmailId = await UserModel.findOne({ email: user.email });
+      const userWithSameEmailId = await UserModel.findOne({
+        email: user.email,
+      });
 
       if (userWithSameEmailId) {
         res.json(RESPONSE.CONFLICTING_RESOURCE);
@@ -40,10 +43,43 @@ router.route(`/`).post(async (req, res) => {
             return;
           }
 
+          // create default playlists for user i.e. history and watch later when the user signs up
+
+          const history = {
+            owner: savedUser._id,
+            name: "history",
+            isDefault: true,
+            type: "history",
+            videos: [],
+          };
+          const watchlater = {
+            owner: savedUser._id,
+            name: "watchlater",
+            isDefault: true,
+            type: "watchlater",
+            videos: [],
+          };
+
+          const liked = {
+            owner: savedUser._id,
+            name: "liked",
+            isDefault: true,
+            type: "liked",
+            videos: [],
+          }
+
+          await new PlaylistModel(history).save();
+          await new PlaylistModel(watchlater).save();
+          await new PlaylistModel(liked).save();
+
           res.json({
             success: true,
             message: `User Registered Successfully`,
-            user: savedUser,
+            user: {
+              name: savedUser.name,
+              email: savedUser.email,
+              avatar: savedUser.avatar,
+            },
             token,
           });
         } catch (error) {
