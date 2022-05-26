ce4bca8dccdb1fe96048ca55d5543374e2123103
unknown <jainprajwal123@gmail.com>
2022-05-26 17:15:39 +0530

    feat: add notes to video routes

--- routers/videos.router.js
+++ routers/videos.router.js
@@ -4,6 +4,7 @@ const { VideoModel } = require("../models/video.model");
 const { UserModel } = require("../models/user.model");
 const { RESPONSE } = require("../utils/common.utils");
 const { authVerify } = require("../middlewares/auth.middleware");
+const { NoteModel } = require("../models/note.model");
 
 router
   .route(`/`)
@@ -145,4 +146,120 @@ router
     }
   });
 
+router
+  .route(`/:videoId/notes`)
+  .get(authVerify, async (req, res) => {
+    const {
+      params: { videoId },
+      user,
+    } = req;
+
+    try {
+      const notes = await NoteModel.find({ video: videoId, user: user._id });
+      res.json({
+        status: 200,
+        success: true,
+        message: `notes fetched successfully from DB`,
+        notes,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `somehting went wrong while fetching notes from DB`,
+        errorMessage: error.message,
+      });
+    }
+  })
+  .post(authVerify, async (req, res) => {
+    const {
+      params: { videoId },
+      body: { note },
+      user
+    } = req;
+    if (!note) {
+      res.json({
+        ...RESPONSE.MALFORMED_SYNTAX,
+        message: `Invalid note sent`,
+      });
+      return;
+    }
+
+    try {
+      note.user = user._id;
+      note.video = videoId;
+      const savedNote = await new NoteModel({ ...note }).save();
+      res.json({
+        status: 201,
+        success: true,
+        message: `note created succesfully`,
+        note: savedNote,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `somehtibng went wrong while saving note to the DB`,
+        errorMessage: error.message,
+      });
+    }
+  });
+
+router
+  .route(`/:videoId/notes/:noteId`)
+  .get( async (req, res) => {
+    const {
+      params: { noteId },
+      user,
+    } = req;
+
+    try {
+      const note = await NoteModel.find({ _id: noteId });
+      res.json({
+        status: 200,
+        success: true,
+        message: `note fetched successfully`,
+        note,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `somehting went wrong while fethcing note from DB`,
+        errorMessage: error.message,
+      });
+    }
+  })
+  .post(async (req, res) => {
+    const {
+      params: { videoId, noteId },
+      user,
+      body: { note },
+    } = req;
+
+    if (!note) {
+      res.json({
+        ...RESPONSE.MALFORMED_SYNTAX,
+        message: `Invalid note sent`,
+      });
+
+      return;
+    }
+    try {
+      const updatedNote = await NoteModel.findOneAndUpdate(
+        { _id: noteId },
+        { ...note },
+        { new: true }
+      );
+
+      res.json({
+        status: 200,
+        message: `Note updated successfully`,
+        note: updatedNote,
+      });
+    } catch (error) {
+      res.json({
+        ...RESPONSE.INTERNAL_SERVER_ERROR,
+        message: `something went wrong while updating the note`,
+        errorMessage: error.message,
+      });
+    }
+  });
 module.exports = { router };
