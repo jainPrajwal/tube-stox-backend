4502f3561a9aaaf7605616b999c511b64ecbb3a5
unknown <jainprajwal123@gmail.com>
2022-06-08 10:35:46 +0530

    chore: package json

--- routers/login.router.js
+++ routers/login.router.js
@@ -1,4 +1,5 @@
 const express = require(`express`);
+const { loginUser } = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.route("/").post(loginUser());
--- routers/signup.router.js
+++ routers/signup.router.js
@@ -1,4 +1,5 @@
 const express = require(`express`);
+const { signupUser } = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.route(`/`).post(signupUser());
