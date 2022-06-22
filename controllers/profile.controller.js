53855f784c44124b136a90bbee7e4e0796df86e0
unknown <jainprajwal123@gmail.com>
2022-06-22 22:59:19 +0530

    feat: update profile route and handler

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,4 +1,7 @@
+const { PaymentModel } = require("../models/payment.model");
 const { PlaylistModel } = require("../models/playlist.model");
+const { UserModel } = require("../models/user.model");
+const { VideoModel } = require(`../models/video.model`);
 const { RESPONSE } = require("../utils/common.utils");
 
 function getProfileHandler() {
@@ -23,13 +26,13 @@ function getProfileHandler() {
         status: 200,
         success: true,
         message: `Playlists and Published Videos fetched Successfully`,
-        playlists: createdPlaylists,
-        videos: publishedVideos,
         user: {
           name: user.name,
           email: user.email,
           avatar: user.avatar,
           isAPremiumMember: user.isAPremiumMember,
+          publishedVideos,
+          gender: user.gender,
         },
       });
     } catch (error) {
@@ -42,4 +45,79 @@ function getProfileHandler() {
   };
 }
 
-module.exports = { getProfileHandler };
+function updateProfileHandler() {
+  return async (req, res) => {
+    const {
+      user,
+      body: { profile },
+    } = req;
+    if (profile?.isAPremiumMember) {
+      try {
+        const foundPayment = await PaymentModel.find({ user: user._id });
+        if (!foundPayment) {
+          res.status(404).json({
+            ...RESPONSE.NOT_FOUND,
+            message: `payment details not found`,
+          });
+          return;
+        }
+      } catch (error) {
+        res.status(500).json({
+          status: 500,
+          success: false,
+          message: `something went wrong while fetching payment details`,
+          errorMessage: error.message,
+        });
+      }
+
+      try {
+        const updatedProfile = await UserModel.findOneAndUpdate(
+          {
+            _id: user._id,
+          },
+          {
+            ...profile,
+          },
+          {
+            new: true,
+          }
+        );
+        try {
+          const publishedVideos = await VideoModel.find({
+            publisher: user._id,
+          });
+          res.json({
+            status: 200,
+            success: true,
+            message: `profile updated successfully`,
+            profile: {
+              name: updatedProfile.name,
+              avatar: updatedProfile.avatar,
+              email: updatedProfile.email,
+
+              isAPremiumMember: updatedProfile.isAPremiumMember,
+              publishedVideos,
+            },
+          });
+        } catch (error) {
+          res.json({
+            ...RESPONSE.INTERNAL_SERVER_ERROR,
+            message: `something went wrong while fetching videos published by you`,
+            errorMessage: error.message,
+          });
+        }
+      } catch (error) {
+        console.error(`error`, error);
+        res.status(500).json({
+          status: 500,
+          success: false,
+          message: `something went wrong while upading the user profile`,
+          errorMessage: error.message,
+        });
+      }
+    }
+  };
+}
+
+module.exports = { getProfileHandler, updateProfileHandler };
+
--- routers/profile.router.js
+++ routers/profile.router.js
@@ -1,11 +1,11 @@
 const express = require(`express`);
-const { getProfileHandler } = require("../controllers/profile.controller");
-const { PlaylistModel } = require("../models/playlist.model");
-const { VideoModel } = require("../models/video.model");
-const { RESPONSE } = require("../utils/common.utils");
+const { getProfileHandler, updateProfileHandler } = require("../controllers/profile.controller");
 const router = express.Router();
 
-router.route(`/`).get(getProfileHandler());
+router
+  .route(`/`)
+  .get(getProfileHandler())
+  .post(updateProfileHandler());
 
 module.exports = { router };
 
